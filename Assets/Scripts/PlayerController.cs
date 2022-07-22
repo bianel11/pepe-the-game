655008f64c01db23a44ab5f4016e8957cc550951
Bianel <bianel9815@gmail.com>
2022-07-22 01:20:24 -0400

    add animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody player;
-
+    private Animator anim;
     public float playerVelocity;
 
     public float playerRotationSpeed;
+
+    // private string iddle = "Idle_Normal_SwordAndShield";
+    // private string running = "MoveBWD_Battle_InPlace_SwordAndShield";
+
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody>();
-
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,18 +31,26 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         // Movements vars
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isAttack", false);
+
         float movH = Input.GetAxis("Horizontal");
         float movV = Input.GetAxis("Vertical");
+
 
         Vector3 movement = new Vector3(movH, 0.0f, movV);
         if (movement != Vector3.zero)
         {
-            print("girando");
+            anim.SetBool("isRunning", true);
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.2f);
         }
 
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Attaque");
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isAttack", true);
+        }
         if (movement != Vector3.zero) transform.Translate(movement * playerVelocity * Time.deltaTime, Space.World);
-
-
     }
 }
